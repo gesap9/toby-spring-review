d73b6c1d95d92ab68941194dbe1d837562c70494
gesap <gesap9@gmail.com>
2017-04-19 14:56:47 +0900

    Done :  - main 함수를 UserDaoTest Class 로 별도로 빼서 구현  - UserDao의 생성자에 ConnectionMaker interface 를 파라메터로 받도록 변경 Ref:  - 1.3 DAO의 확장  - 1.3.3 관계설정 책임의 분리(~P83)  - 1.3.4 원칙과 패턴(~P87) Text :  - 변화사항   . main 함수를 Client로 삼아 UserDao와 ConnectionMaker의 관계설정을 해 줌  - 개방 폐쇄 원칙(OCP, Open-Closed Principle)   . 클래스나 모듈은 확장에는 열려 있어야 하고 변경에는 닫혀 있어야 한다.  - 전략 패턴   . 자신의 기능 맥락에서 필요에 따라 변경이 필요한 알고리즘을 인터페이스를     통해 통째로 외부로 분리시키고, 이를 구현한 구체적인 알고리즘 클래스를     필요에 따라 바꿔서 사용할 수 있게 하는 디자인 패턴   . UserDao는 전략패턴의 컨텍스트에 해당   . ConnectionMaker의 구현 클래스 = 전략   . 컨텍스트(UserDao)를 사용하는 클라이언트(UserDaoTest)는 컨텍스트가 사용할     전략(ConnectionMaker의 구현클래스, e.g. DConnectionMaker)을 컨텍스트의     생성자 등을 통해 제공해주는 것이 일반적

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -11,8 +11,8 @@ import java.sql.*;
 public class UserDao {
     private ConnectionMaker connectionMaker;
 
-    public UserDao(){
-        connectionMaker = new DConnectionMaker(); //Class 이름이 나옴
+    public UserDao(ConnectionMaker connectionMaker){
+        this.connectionMaker = connectionMaker; //Class 이름이 나옴
     }
 
     public void add(User user) throws SQLException, ClassNotFoundException {
@@ -51,21 +51,5 @@ public class UserDao {
 
         return user;
 
-    }
-
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new UserDao();
-
-        User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
-        dao.add(user);
-        System.out.println(user.getId() + " 등록 성공");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-        System.out.println(user2.getId() + " 조회 성공");
     }
 }
