5c044443f829aed5e8bc1f6e543fe98332cfe9d3
gesap <gesap9@gmail.com>
2017-04-19 16:00:53 +0900

    Done :  - Spring의 설정정보로 사용할 수 있도록 DaoFactory에 관련 애노테이션 추가  - UserDaoTest에서 Dao를 ApplicationContext에서 가져오도록 변경  - spring 관련 gradle dependency 추가 Ref:  - 1.5 스프링의 IoC  - 1.5.1 오브젝트 팩토리를 이용한 스프링 IoC(~P98)  - 1.5.2 애플리케이션 컨텍스트의 동작방식(~P100)  Text :  - Object Factory와 Spring의 Application Context가 대응됨  - Application Context를 IoC 컨테이너, 스프링 컨테이너, 빈팩토리 등으로 불림  - ApplicationContext->BeanFactory  - DaoFactory를 오브젝트 팩토리로 직접 사용했을 때와 비교해서 애플리케이션 컨텍스트를 사용시 장점   . 클라이언트는 구체적인 팩토리 클래스를 알 필요가 없음   . 오브젝트생성, 관계설정 뿐만 아니라 만들어지는 방식, 시점과 전략,     자동생성, 후처리, 정보의 조합, 설정방식의 다변화, 인터셉팅 등 기능 제공   . 다양한 빈 검색방법(빈이름, 타입, 애노테이션 등)

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,16 +1,22 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author Jeongjin Kim
  * @since 2017-04-19
  */
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao(){
         UserDao userDao = new UserDao(connectionMaker());
 
         return userDao;
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
